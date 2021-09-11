6023ba5044e1fb89fcc8da8ab9adf776ed083d2e
kimsehw <seunghwan9703@gmail.com>
2021-09-11 16:59:38 +0900

    add class(add)

---
+++ add.java
@@ -0,0 +1,13 @@
+public class add{	
+	int sum(int x,int y){
+	int result = x+y;
+	return result;
+}
+
+
+
+	public static void main(String[]args){
+		add myAdd = new add();
+		System.out.print(myAdd.sum(3,4));
+	}
+}
